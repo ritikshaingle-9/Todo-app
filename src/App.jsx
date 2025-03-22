a459a8af98c632726c402faa6921b696bc3970d2
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-03-22 13:57:20 +0530

    page created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,99 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import TodoCard from './component/Todo-card';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
+  const [todoItem, setTodoItem] = useState({
+    task: "",
+    priority: ""
+  });
+
+  const [todoList, setTodoList] = useState(() => {
+    return JSON.parse(localStorage.getItem("todoList")) || [];
+  });
+
+  const [selectedTab, setSelectedTab] = useState("All");
+
+  useEffect(() => {
+    if (todoList.length === 0) return;
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  const onDelete = (index) => {
+    const listAfterDeletion = todoList.filter((_, i) => i !== index);
+    setTodoList(listAfterDeletion);
+  };
+
+  const filteredTodoList = selectedTab === "All" 
+    ? todoList 
+    : todoList.filter(taskItem => taskItem.priority === selectedTab);
+
   return (
-    <div>App</div>
-  )
+    <div className='bg-amber-50 min-h-screen'>
+      <div className='flex justify-around border border-b-2 border-slate-400 pt-4'>
+        {["All", "High", "Medium", "Low"].map((tab, i) => {
+          return (
+            <span 
+              className={`py-1 text-lg md:text-xl text-center w-[100px] md:w-[250px] rounded-tl-lg rounded-tr-lg block cursor-pointer ${tab === selectedTab ? "bg-slate-400 text-white" : "bg-white"}`} 
+              key={i}  
+              onClick={() => setSelectedTab(tab)}  
+            >
+              {tab}
+            </span>
+          );
+        })}
+      </div>
+
+      <div className='h-[60vh] md:h-[80vh] overflow-y-scroll'>
+        {filteredTodoList.map((taskItem, index) => {
+          const { task, priority } = taskItem;
+
+          return (
+            <TodoCard
+              task={task}
+              priority={priority}
+              key={index}
+              index={index}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+
+      <div className='fixed bottom-0 left-0 w-full bg-slate-400 p-10 flex md:flex-row flex-col items-center justify-center gap-y-4'>
+        <input
+          type="text"
+          onChange={(e) => {
+            setTodoItem({
+              ...todoItem,
+              task: e.target.value
+            });
+          }}
+          value={todoItem.task}
+          className='bg-white text-xl md:w-[400px] rounded-md p-2 focus:outline-none w-[300px]'
+          placeholder='Enter Task'
+        />
+
+        <select
+          className='text-xl bg-white px-5 py-2 rounded-md ml-0 md:ml-5 md:w-[200px]'
+          onChange={(e) => {
+            setTodoItem({
+              ...todoItem,
+              priority: e.target.value
+            });
+          }}
+          value={todoItem.priority}
+        >
+          <option value="">Select Priority</option>
+          <option value="High">High</option>
+          <option value="Medium">Medium</option>
+          <option value="Low">Low</option>
+        </select>
+
+      </div>
+      <Toaster />
+    </div>
+  );
 }
 
-export default App
+export default App;
